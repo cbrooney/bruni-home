fae7d76c0b045b2f24fcd53a5cc416be0f7a3eb1
Cristian Bruni <bruni@auxmoney.com>
2022-08-26 21:21:37 +0200

    slideshow: add anton style slideshow

--- src/Controller/SlideShowController.php
+++ src/Controller/SlideShowController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -56,6 +57,49 @@ class SlideShowController extends AbstractController
 
         return $this->render('slideshow/slides.html.twig', [
             'number' => $number,
+            'directoryEntries' => $directoryEntries,
+        ]);
+    }
+
+    /**
+     * @Route("/slideshow/get-figure", name="slide_show_figure", methods={"GET"})
+     */
+    public function slidesFigure(): Response
+    {
+        $fileTypes = ["JPG","jpg", "jpeg","webm"];
+        $fileNameList = [];
+
+        $directoryEntries = scandir($this->bilderDir);
+        $directoryEntries = array_diff(
+            $directoryEntries,
+            ['..', '.', '.gitkeep']
+        );
+
+        shuffle($directoryEntries);
+
+        return new JsonResponse(
+            ['filename' => $directoryEntries[0]]
+        );
+    }
+
+    /**
+     * @Route("/slideshow-a", name="slide_show_a", methods={"GET"})
+     */
+    public function slidesFigureA(): Response
+    {
+        $fileTypes = ["JPG","jpg", "jpeg","webm"];
+        $fileNameList = [];
+
+        $directoryEntries = scandir($this->bilderDir);
+        $directoryEntries = array_diff(
+            $directoryEntries,
+            ['..', '.', '.gitkeep']
+        );
+
+        $entries = [];
+        $entries[] = $directoryEntries[4];
+
+        return $this->render('slideshow/slides-a.html.twig', [
             'directoryEntries' => $directoryEntries,
         ]);
     }
