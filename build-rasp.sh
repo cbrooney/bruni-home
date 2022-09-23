40c87d09a6851d3b230ab503a490632f1eb2224b
Cristian Bruni <bruni@auxmoney.com>
2022-09-23 20:53:39 +0200

    docker-rasp: first test building docker container on rasp

---
+++ build-rasp.sh
@@ -0,0 +1,2 @@
+docker build . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="arm32v7/php:7.4.30-fpm-bullseye" --tag ghcr.io/cbrooney/bruni-home-php-fpm-rasp:test
+docker build . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="arm32v7/nginx:stable-alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
