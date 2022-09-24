90dfa9fac3447ddec95017aef492d3e8133c8ec9
Cristian Bruni <bruni@auxmoney.com>
2022-09-24 09:14:43 +0200

    docker-rasp: test with hash

--- build-rasp.sh
+++ build-rasp.sh
@@ -1,2 +1,2 @@
-docker build . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="arm32v7/php:7.4.30-fpm-bullseye" --tag ghcr.io/cbrooney/bruni-home-php-fpm-rasp:test
-docker build . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="arm32v7/nginx:stable-alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
+#docker build . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="arm32v7/nginx:stable-alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
+docker build . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-bullseye:6b9ed33b1b393e6c3ebe8c0e7a8a9fe34264ed16b6dc31518248805f8abedbb8" --tag ghcr.io/cbrooney/bruni-home-php-fpm-rasp:test
