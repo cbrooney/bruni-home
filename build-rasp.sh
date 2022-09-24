0c4adac288728457cb47d915a8f912ce1fe2bf6c
Cristian Bruni <bruni@auxmoney.com>
2022-09-24 16:15:44 +0200

    docker-rasp: build nginx image for rasp

--- build-rasp.sh
+++ build-rasp.sh
@@ -1,2 +1,2 @@
-#docker build . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="arm32v7/nginx:stable-alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
+docker build --platform linux/arm/v7 . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="nginx:alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
 docker build --platform linux/arm/v7 . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --tag ghcr.io/cbrooney/bruni-home-php-fpm-rasp:test
