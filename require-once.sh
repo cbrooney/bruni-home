dd82beb23cdf4b8119427c9a7f719eb2ce2f8228
Cristian Bruni <bruni@auxmoney.com>
2022-09-27 07:57:41 +0200

    slideshow-files: test mounting symlink

--- require-once.sh
+++ require-once.sh
@@ -2,5 +2,7 @@ touch ./docker/php-fpm/.bash_history
 
 cp .env.local.docker-compose.dist .env.local.docker-compose
 
+rm ./public/bilder
 ln -sT /media/usb/netzwerk/share/raspi ./public/bilder
 
+# ls -la public zeigt: bilder -> /media/usb/netzwerk/share/rasp
