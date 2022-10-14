3eb4f7f1e0340dbb11cd9717f0da28cdca157c4a
Cristian Bruni <bruni@auxmoney.com>
2022-10-14 15:05:15 +0200

    add-tests: fix permission issues in docker

--- require-once.sh
+++ require-once.sh
@@ -1,9 +1,10 @@
 touch ./docker/php-fpm/.bash_history
-chown 0777 ./docker/php-fpm/.bash_history
+sudo chown 0777 ./docker/php-fpm/.bash_history
 touch .env.local
 
 cp .env.local.docker-compose.dist .env.local.docker-compose
 
+# only live
 rm ./public/bilder
 ln -sT /media/usb/netzwerk/share/raspi ./public/bilder
 
