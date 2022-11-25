0c8dc5970979b2825f088361b568f22ee9539311
Cristian Bruni <bruni@auxmoney.com>
2022-11-25 09:21:52 +0100

    add-tests: add first working version of test

--- require-once.sh
+++ require-once.sh
@@ -7,5 +7,6 @@ cp .env.local.docker-compose.dist .env.local.docker-compose
 # only live
 rm ./public/bilder
 ln -sT /media/usb/netzwerk/share/raspi ./public/bilder
+#ln -sf
 
 # ls -la public zeigt: bilder -> /media/usb/netzwerk/share/rasp
