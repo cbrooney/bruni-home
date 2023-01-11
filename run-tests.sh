36b08cf2d1f1c0f35503780b1f4ce946a1e48b40
Cristian Bruni <bruni@auxmoney.com>
2023-01-11 11:46:43 +0100

    add-tests: next test with pushing test php fpm image

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
-docker-compose -f docker-compose-db.yml -f docker-compose.yml --env-file .env.test up -d --build
+docker-compose -f docker-compose-db.yml -f docker-compose.yml -f docker-compose-php-test.yml --env-file .env.test up -d
 
-docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
+docker-compose -f docker-compose-php-test.yml --env-file .env.test run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
 
-docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm composer tests
+docker-compose -f docker-compose-php-test.yml --env-file .env.test run php-fpm composer tests
 
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down
+docker-compose -f docker-compose-db.yml -f docker-compose.yml -f docker-compose-php-test.yml --env-file .env.test down
