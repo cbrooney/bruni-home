1666e75b83bcb0b9fc4315adea1bfbc3586b7681
Cristian Bruni <bruni@auxmoney.com>
2023-01-11 10:07:34 +0100

    add-tests: change env file

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose-db.yml -f docker-compose.yml --env-file .env.local.docker-compose up -d --build
+docker-compose -f docker-compose-db.yml -f docker-compose.yml --env-file .env.test up -d --build
 
 docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
 
