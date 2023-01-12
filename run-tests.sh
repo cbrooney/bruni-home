0a342eab6434221ffc569ebf9f9782a667cba8d2
Cristian Bruni <bruni@auxmoney.com>
2023-01-12 15:56:28 +0100

    add-tests: add port

--- run-tests.sh
+++ run-tests.sh
@@ -9,4 +9,4 @@ docker-compose -f docker-compose-db.yml -f docker-compose.yml --env-file .env.lo
 docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
 
 docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm composer tests
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down
+docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
