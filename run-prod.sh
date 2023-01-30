adfb6d8d415e7c2b0241625816966d16d4f6a653
Cristian Bruni <bruni@auxmoney.com>
2023-01-30 14:54:00 +0100

    multi-stage: try changing user id

--- run-prod.sh
+++ run-prod.sh
@@ -1,7 +1,9 @@
 # build prod env
-docker build . -f docker/php-fpm/build/Dockerfile-prod --build-arg APP_ENV="prod"  --tag ghcr.io/cbrooney/bruni-home-app-prod:test-local
-docker build . -f docker/php-fpm/build/Dockerfile-prod --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --build-arg APP_ENV="prod"  --tag ghcr.io/cbrooney/bruni-home-app-prod:test-local
-docker push ghcr.io/cbrooney/bruni-home-app-prod:test-local
+#docker build . -f docker/php-fpm/build/Dockerfile-prod --build-arg APP_ENV="prod" --build-arg CONTAINER_USER_UID="1000" --tag ghcr.io/cbrooney/bruni-home-app-prod:test-local
+#docker push ghcr.io/cbrooney/bruni-home-app-prod:test-local
 
 # run prod env
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d
+docker-compose -f docker-compose-prod.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d
+
+# down
+# docker-compose -f docker-compose-prod.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down
