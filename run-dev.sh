c1a4219c31773e6c19b640e9f2ab5411eaee7138
Cristian Bruni <bruni@auxmoney.com>
2023-01-30 15:46:28 +0100

    multi-stage: publish latest prod after main merge

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,5 @@
 # build dev env
-docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-env
+# docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-env
 
-# run dev env
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
-
-#
+# run dev env, and build
+docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@
 #docker-compose -f docker-compose-db.yml -f docker-compose-php-test.yml --env-file .env.test down
 
 # run locally
-docker-compose -f docker-compose-db.yml -f docker-compose.yml --env-file .env.local.docker-compose up -d
-docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
-docker-compose -f docker-compose.yml --env-file .env.local.docker-compose run php-fpm composer tests
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
+docker-compose -f docker-compose-db.yml -f docker-compose-dev.yml --env-file .env.local.docker-compose up -d
+docker-compose -f docker-compose-dev.yml --env-file .env.local.docker-compose run php-fpm utils/wait-for-it.sh mysql:3306 -t 60
+docker-compose -f docker-compose-dev.yml --env-file .env.local.docker-compose run php-fpm composer tests
+docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
