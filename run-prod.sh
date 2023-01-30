c33c3b51205a89ced3b4b8e5ed8b1643683bd454
Cristian Bruni <bruni@auxmoney.com>
2023-01-30 13:54:53 +0100

    multi-stage: try creating prod env 2

--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,7 @@
-# run dev env
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
+# build prod env
+docker build . -f docker/php-fpm/build/Dockerfile-prod --build-arg APP_ENV="prod"  --tag ghcr.io/cbrooney/bruni-home-app-prod:test-local
+docker build . -f docker/php-fpm/build/Dockerfile-prod --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --build-arg APP_ENV="prod"  --tag ghcr.io/cbrooney/bruni-home-app-prod:test-local
+docker push ghcr.io/cbrooney/bruni-home-app-prod:test-local
 
-#
+# run prod env
+docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d
