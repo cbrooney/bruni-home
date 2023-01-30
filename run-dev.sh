97f590cb2069b2ed67cadcc09c5ad9a013dce656
Cristian Bruni <bruni@auxmoney.com>
2023-01-30 12:22:41 +0100

    multi-stage: move docker base images, try running tests with this repo and specific branch

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,7 @@
-docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d
+# build dev env
+docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-env
+
+# run dev env
+docker-compose -f docker-compose.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
 
 #
