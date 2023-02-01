997b902185fd9a2b84ca03d345039d4797927354
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 15:45:36 +0100

    prod-rasp: build-rasp.sh

--- run-rasp.sh
+++ run-rasp.sh
@@ -1,9 +1,8 @@
-# build dev env
-# docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-env
-
 # run dev env, and build
 docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
 
 docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --remove-orphans
 
 docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
+
+docker compose -f docker-compose-rasp.yml --env-file .env.local.docker-compose down --remove-orphans
