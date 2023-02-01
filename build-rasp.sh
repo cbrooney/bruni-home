bef15450c3e5a9dd004be526da4270753ac9d2c2
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 16:14:06 +0100

    prod-rasp: get base image for prod by variable

--- build-rasp.sh
+++ build-rasp.sh
@@ -1,21 +1,6 @@
-# only created locally, not pushed to github
-docker build --platform linux/arm/v7 . -f docker/php-fpm/build/Dockerfile-prod --build-arg APP_ENV="prod" --build-arg CONTAINER_USER_UID="1000" --tag ghcr.io/cbrooney/bruni-home-app-rasp:latest
+# update branch
+git pull
+
+# only created locally on rasp, not pushed to github
+docker build --platform linux/arm/v7 . -f docker/php-fpm/build/Dockerfile-prod --build-arg APP_ENV="prod" --build-arg CONTAINER_USER_UID="1000" --build-arg BASE_IMAGE_PHP="base-php-fpm-rasp:latest" --tag ghcr.io/cbrooney/bruni-home-app-rasp:latest
 docker build --platform linux/arm/v7 . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="nginx:alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:latest
-
-# docker push ghcr.io/cbrooney/bruni-home-app-rasp:latest
-# docker push ghcr.io/cbrooney/bruni-home-nginx-rasp:latest
-
-
-#docker build --platform linux/arm/v7 . -f docker/nginx/build/Dockerfile --build-arg BASE_IMAGE_NGINX="nginx:alpine" --tag ghcr.io/cbrooney/bruni-home-nginx-rasp:test
-#docker build --platform linux/arm/v7 . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --tag ghcr.io/cbrooney/bruni-home-php-fpm-rasp:test
-#
-#
-#docker push ghcr.io/cbrooney/bruni-home-app-rasp:latest
-#
-#
-#docker build --platform linux/arm/v7 . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target prod-base --tag ghcr.io/cbrooney/base-php-fpm-rasp:latest
-#docker push ghcr.io/cbrooney/base-php-fpm-rasp:latest
-#docker build . -f php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-base --tag ghcr.io/cbrooney/base-php-fpm-dev:latest
-#docker push ghcr.io/cbrooney/base-php-fpm-dev:latest
-#
-#
