3dcc3232124bd93d9b4ace28e39c36f6908e3634
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 12:46:38 +0100

    prod-rasp: change compose yml for rasp

---
+++ run-rasp.sh
@@ -0,0 +1,5 @@
+# build dev env
+# docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --target dev-env
+
+# run dev env, and build
+docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
