7ef99cda3af3edc01bea1c528e25dfbafb4f449a
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 15:48:34 +0100

    prod-rasp: run-rasp.sh

--- run-rasp.sh
+++ run-rasp.sh
@@ -1,8 +1,9 @@
-# run dev env, and build
-docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
-
+# run rasp in prod mode
 docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --remove-orphans
 
-docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
+# down only app => causes network error
+#docker compose -f docker-compose-rasp.yml --env-file .env.local.docker-compose down
 
-docker compose -f docker-compose-rasp.yml --env-file .env.local.docker-compose down --remove-orphans
+# down complete
+#docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
+#docker compose -f docker-compose-rasp.yml --env-file .env.local.docker-compose down --remove-orphans
