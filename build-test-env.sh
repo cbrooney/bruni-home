b76faac589e81aed7afaf6964b09e9a892d618d6
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 17:44:51 +0100

    prod-rasp: clean up a little

--- build-test-env.sh
+++ build-test-env.sh
@@ -1,2 +1,3 @@
-docker build . -f docker/php-fpm/build/Dockerfile --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --tag ghcr.io/cbrooney/bruni-home-php-fpm:test-env
-docker push ghcr.io/cbrooney/bruni-home-php-fpm:test-env
+# create image for tests for this branch
+docker build . -f docker/php-fpm/build/Dockerfile-dev --build-arg BASE_IMAGE_PHP="php:7.4.30-fpm-buster" --tag ghcr.io/cbrooney/bruni-home-app-test:${GITHUB_HEAD_REF}
+# docker push ghcr.io/cbrooney/bruni-home-app-test:${GITHUB_HEAD_REF}
