bef7c0c4f102b9957154d901ec1c447b4664a059
Cristian Bruni <bruni@auxmoney.com>
2023-02-01 15:03:34 +0100

    prod-rasp: try using own docker yml for db

--- run-rasp.sh
+++ run-rasp.sh
@@ -3,3 +3,7 @@
 
 # run dev env, and build
 docker-compose -f docker-compose-dev.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --build
+
+docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose up -d --remove-orphans
+
+docker compose -f docker-compose-rasp.yml -f docker-compose-db.yml --env-file .env.local.docker-compose down --remove-orphans
