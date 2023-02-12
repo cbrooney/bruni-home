8f83e816b99db706d868dca5708d1c468dfb7704
Cristian Bruni <bruni@auxmoney.com>
2023-02-12 14:09:16 +0100

    stats: move bash history and mount private dir

--- utils/require-once.sh
+++ utils/require-once.sh
@@ -1,11 +1,16 @@
-touch ./docker/php-fpm/.bash_history
-sudo chown 0777 ./docker/php-fpm/.bash_history
+# touch ./docker/php-fpm/.bash_history
+# sudo chown 0777 ./docker/php-fpm/.bash_history
+# bash history wurde verschoben
+mkdir ../bruni-home-private
+touch ../bruni-home-private/.bash_history
+sudo chown 0777 ../bruni-home-private/.bash_history
+
 touch .env.local
 
 cp .env.local.docker-compose.dist .env.local.docker-compose
 
 # only live
-rm ./public/bilder
+# rm ./public/bilder
 ln -sT /media/usb/netzwerk/share/raspi ./public/bilder
 #ln -sf
 
