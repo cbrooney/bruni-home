daadfe682fc683a0131b3c8daeaaac188a748b6c
Cristian Bruni <bruni@auxmoney.com>
2023-08-10 18:33:38 +0200

    file-manager: start with table and command for file scanning [skip-ci]

---
+++ migrations/Version20230810161113.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230810161113 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            'CREATE TABLE IF NOT EXISTS `file_list` (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `full_path` varchar(300) NOT NULL,
+              `file_name` varchar(100) NOT NULL,
+              `relative_path` varchar(200) NOT NULL,
+              `file_type` varchar(20) DEFAULT NULL,
+              `file_size` int DEFAULT NULL,
+              `hash` varchar(300) DEFAULT NULL,
+              `created_at` datetime NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
---
+++ src/FileManager/App/Command/FileAnalysisCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\FileManager\App\Command;
+
+use Exception;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class FileAnalysisCommand extends Command
+{
+    protected static $defaultName = 'file:analysis';
+
+    private LoggerInterface $logger;
+
+    public function __construct(
+        LoggerInterface $logger
+    ) {
+        parent::__construct();
+        $this->logger = $logger;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument(
+                'directory',
+                InputOption::VALUE_REQUIRED,
+                'The root directory to scan:'
+            )
+            ->setDescription('collect recursively information of data in directory');
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->logger->info('Start');
+
+        try {
+            $inputType = $input->getArgument('directory');
+        } catch (Throwable $exception) {
+            $this->logger->error(sprintf('Error while running Command. Error was: %s', $exception->getMessage()));
+
+            return self::FAILURE;
+        }
+
+        $this->logger->info('End');
+
+        return self::SUCCESS;
+    }
+}
